bf07f4f518a2c9dd409ca60da7442df2d6965007
philipp_sitarskiy <philippsitarskiy@gmail.com>
2022-08-03 15:39:30 +0300

    update server

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express()
 const server = http.createServer(app)
 const io = new Server(server, {
     cors: {
-        origin: 'https://chat-vueee.herokuapp.com/',
+        origin: 'http://localhost:8080/',
         credentials: true,
         methods: ['GET', 'POST']
     }
