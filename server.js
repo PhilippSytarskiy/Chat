6a2fedf851dd1e8baf207934cb883e0ee5fa451b
philipp_sitarskiy <philippsitarskiy@gmail.com>
2022-08-03 14:30:53 +0300

    update port

--- server.js
+++ server.js
@@ -63,6 +63,8 @@ io.on('connection', (socket) => {
 
 })
 
-server.listen(3000, () => {
+const port =  process.env.PORT || 3000;
+
+server.listen(port, () => {
     console.log('Chat server is rinning')
 })
